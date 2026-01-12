7ee8e937cad776462cbea6459cfcb70d7d56f64a
Nikola Golijanin <nikola.golijanin@bsdigital.com>
2026-01-12 23:49:53 +0100

    Add .gitattributes and .gitignore.

---
+++ src/Modules/Attendance/Evently.Modules.Attendance.IntegrationTests/Abstractions/IntegrationTestWebAppFactory.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Testcontainers.PostgreSql;
+using Testcontainers.Redis;
+
+namespace Evently.Modules.Attendance.IntegrationTests.Abstractions;
+
+public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder("postgres:17")
+        .WithDatabase("evently")
+        .WithUsername("postgres")
+        .WithPassword("postgres")
+        .Build();
+
+    private readonly RedisContainer _redisContainer = new RedisBuilder("redis:latest")
+        .Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        Environment.SetEnvironmentVariable("ConnectionStrings:Database", _dbContainer.GetConnectionString());
+        Environment.SetEnvironmentVariable("ConnectionStrings:Cache", _redisContainer.GetConnectionString());
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _dbContainer.StartAsync();
+        await _redisContainer.StartAsync();
+    }
+
+    public new async Task DisposeAsync()
+    {
+        await _dbContainer.StopAsync();
+        await _redisContainer.StopAsync();
+    }
+}
